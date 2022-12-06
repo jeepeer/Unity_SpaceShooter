274737541e5629c2976c1b0c04807eb6357a9fbe
jeepeer <jespervski@gmail.com>
2022-12-06 13:04:16 +0100

    player spaceship movement

--- SpaceShooter/Assets/Scripts/SpaceShip.cs
+++ SpaceShooter/Assets/Scripts/SpaceShip.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SpaceShip : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed;
+   
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleShooting();
+    }
+
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveUp(speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            MoveUp(-speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveRight(-speed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            MoveRight(speed);
+        }
+    }
+
+    void MoveUp(float value)
+    {
+        transform.Translate(Vector3.up * value * Time.deltaTime);
+    }
+    void MoveRight(float value)
+    {
+        transform.Translate(Vector3.right * value * Time.deltaTime);
+    }
+
+    void HandleShooting()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        // add bullet to array, 
+        // bullet array moves the bullets ?
     }
 }
